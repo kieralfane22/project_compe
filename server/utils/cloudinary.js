9549af32ea2ff6053bc522a56d19793df1ae3efc
Myke Kieran Alfane <mykekierkier@gmail.com>
2025-06-18 04:15:51 +0800

    cloudinary fix

--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -1,7 +1,12 @@
 import { v2 as cloudinary } from 'cloudinary';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Loads .env variables
 
 cloudinary.config({
-  cloud_name: 'YOUR_CLOUD_NAME',
-  api_key: 'YOUR_API_KEY',
-  api_secret: 'YOUR_API_SECRET',
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+export default cloudinary;
